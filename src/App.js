c5aef0c43154ff4164d5e41635bceec42e14b487
Milena Souto <yamadasouto@gmail.com>
2022-12-13 14:42:06 -0800

    Update App by importing ChatEntry component and adding element and props

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 
+import ChatEntry from './components/ChatEntry';
+
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        {/* TODO: add names instead of hardcoding */}
+        <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        {/* Wave 01: Render one ChatEntry component */}
+        <ChatEntry messages={chatMessages} />
+
+        {/* Wave 02: Render ChatLog component  */}
       </main>
     </div>
   );
