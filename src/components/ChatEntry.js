fe8aea85d4ca3c67b627ea7b2c4a430f0490d58d
Milena Souto <yamadasouto@gmail.com>
2022-12-14 17:43:47 -0800

    Pass last wave 1 test for getting timeStamp value with TimeStamp function

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -2,8 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './ChatEntry.css';
+import TimeStamp from './TimeStamp';
 
 const ChatEntry = ({ body, sender, timeStamp }) => {
+  // TODO: calculate years ('years ago')
+  const entryTime = TimeStamp(timeStamp);
+  // TimeStamp expecting (props)
+
+  // my code for getting get values from TIMESTAMP and JS current time
+  // const currentTimeStamp = new Date();
+  // const currentYear = currentTimeStamp.getFullYear();
+  // const entryTimeStamp = timeStamp;
+  // const newEntryTimeStamp = entryTimeStamp.split(/[- :]/);
+  // const entryYear = newEntryTimeStamp[0];
+  // const difference = currentYear - entryYear;
+
   return (
     <div className="chat-entry local">
       {/* <h2 className="entry-name">Replace with name of sender</h2> */}
@@ -13,8 +26,9 @@ const ChatEntry = ({ body, sender, timeStamp }) => {
         {/* <p>Replace with body of ChatEntry</p> */}
         {body}
 
+        <p className="entry-time">{entryTime}</p>
         {/* <p className="entry-time">Replace with TimeStamp component</p> */}
-        <p className="entry-time">{timeStamp}</p>
+        {/* <p className="entry-time">{difference}</p> */}
 
         {/* {chatLike} */}
         <button className="like">🤍</button>
